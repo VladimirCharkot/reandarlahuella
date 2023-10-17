6a4b5c7eed47ae1fe61720b4f8371ee60884a576
VladimirCharkot <vlad.chk@gmail.com>
2023-10-17 16:35:50 -0300

    Probando bot sin polling

--- src/app/lib/tg.ts
+++ src/app/lib/tg.ts
@@ -4,14 +4,15 @@ const token = process.env.TG_BOT_TOKEN;
 
 if(!token) throw new Error("Falta el token de telegram")
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token)
+// , {polling: true});
 
 console.log(`Inicializando bot...`)
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, `@chatId ${chatId}: Received your message`);
-});
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//   bot.sendMessage(chatId, `@chatId ${chatId}: Received your message`);
+// });
 
 
 export default bot
