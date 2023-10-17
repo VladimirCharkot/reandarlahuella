e156fb6bb8f9e80a99f14e791f110a22ab132459
VladimirCharkot <vlad.chk@gmail.com>
2023-10-17 16:47:52 -0300

    Regresando el return fuera del then

--- src/app/wu/route.ts
+++ src/app/wu/route.ts
@@ -9,8 +9,8 @@ export const POST = async (req: Request, res: Response) => {
     bot.sendMessage(process.env.TG_CHAT_ID!, `${body.nombre} (${body.mail}) envió un giro de WU por USD$${body.monto} desde ${body.pais} con el MTCN ${body.mtcn}`)
       .then(() => {
         console.log(`Enviado mensaje a tg por ${body.nombre} (${body.mail}) USD$${body.monto} desde ${body.pais} con el MTCN ${body.mtcn}`)
-        return NextResponse.json({ ok: true })
       })
+    return NextResponse.json({ ok: true })
   } catch (e: any) {
     return NextResponse.json({ ok: false, msg: e.message })
   }
