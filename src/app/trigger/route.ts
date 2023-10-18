a90514563e50d71cac3e5adbe38a515a631d06f1
VladimirCharkot <vlad.chk@gmail.com>
2023-10-18 16:27:08 -0300

    async/await?

--- src/app/trigger/route.ts
+++ src/app/trigger/route.ts
@@ -7,16 +7,21 @@ export const POST = async (req: Request, res: Response) => {
   if (b.password == process.env.TRIGGER_PASSWORD) {
     console.log(`Enviando mail...`)
     bot.sendMessage(process.env.TG_CHAT_ID!, `Enviando mails y pdfs a ${b.nombre} (${b.mail})...`)
-    enviarReandarYVidriera(b.mail, b.nombre)
-      .then(
-        r => {
-          console.log(`...mail enviado!`)
-          bot.sendMessage(process.env.TG_CHAT_ID!, `...enviado a ${b.nombre} (${b.mail})! ${r}`)
-        })
-      .catch(e => {
-        console.log(`...falló envío!`)
-        bot.sendMessage(process.env.TG_CHAT_ID!, `...falló envío a ${b.nombre} (${b.mail})! ${e}`)
-      })
+    try{
+      const r = await enviarReandarYVidriera(b.mail, b.nombre)
+      bot.sendMessage(process.env.TG_CHAT_ID!, `...enviado a ${b.nombre} (${b.mail})! ${r}`)
+    }catch(e){
+      bot.sendMessage(process.env.TG_CHAT_ID!, `...falló envío a ${b.nombre} (${b.mail})! ${e}`)
+    }
+      // .then(
+      //   r => {
+      //     console.log(`...mail enviado!`)
+      //     bot.sendMessage(process.env.TG_CHAT_ID!, `...enviado a ${b.nombre} (${b.mail})! ${r}`)
+      //   })
+      // .catch(e => {
+      //   console.log(`...falló envío!`)
+      //   bot.sendMessage(process.env.TG_CHAT_ID!, `...falló envío a ${b.nombre} (${b.mail})! ${e}`)
+      // })
     return NextResponse.json({ ok: true })
   } else {
     return NextResponse.json({ ok: false, msg: `No autorizado c:` })
