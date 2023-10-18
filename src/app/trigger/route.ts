f2c34f25a5113ad4f0fd915c21f5bd92f704cb3f
VladimirCharkot <vlad.chk@gmail.com>
2023-10-18 09:59:42 -0300

    catch that bitch

--- src/app/trigger/route.ts
+++ src/app/trigger/route.ts
@@ -8,10 +8,19 @@ export const POST = async (req: Request, res: Response) => {
   if(b.password == process.env.TRIGGER_PASSWORD){
     console.log(`Enviando mail...`)
     bot.sendMessage(process.env.TG_CHAT_ID!, `Enviando mails y pdfs a ${b.nombre} (${b.mail})`).then( () => console.log(`Mensaje enviado a Tg`) )
-    emailer.enviarReandar(b.mail, b.nombre).then(() => {
-      console.log(`Reandar enviado`)
-      bot.sendMessage(process.env.TG_CHAT_ID!, `Mail y pdfs enviados a ${b.nombre} (${b.mail})`)
-    })
+    const p = emailer.enviarReandar(b.mail, b.nombre)
+    p
+      .then(r => {
+        bot.sendMessage(process.env.TG_CHAT_ID!, `Mail salió!`)
+      })
+      .catch(r => {
+        bot.sendMessage(process.env.TG_CHAT_ID!, `Mail falló: ${r}`)
+      })
+    
+    // emailer.enviarReandar(b.mail, b.nombre).then(() => {
+    //   console.log(`Reandar enviado`)
+    //   bot.sendMessage(process.env.TG_CHAT_ID!, `Mail y pdfs enviados a ${b.nombre} (${b.mail})`)
+    // })
     return NextResponse.json({ok: true})
   }else{
     return NextResponse.json({ok: false, msg: `No autorizado c:`})
