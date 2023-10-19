815077ea1e5342e02027b2ac6686da1fb9fb719d
VladimirCharkot <vlad.chk@gmail.com>
2023-10-19 15:26:20 -0300

    Trim textos

--- src/components/txtInput.tsx
+++ src/components/txtInput.tsx
@@ -7,7 +7,7 @@ interface TxtInputProps{
 
 const TxtInput = ({ nombre, valor, setValor }: TxtInputProps) => <>
   <label className='mt-2' htmlFor={nombre}>{capitalize(nombre)}:</label>
-  <input className="bg-slate-700 p-2 text-center mb-4" type="text" name={nombre} value={valor} onChange={e => setValor(e.target.value)}/>
+  <input className="bg-slate-700 p-2 text-center mb-4" type="text" name={nombre} value={valor} onChange={e => setValor(e.target.value.trim())}/>
 </>
 
 export default TxtInput
